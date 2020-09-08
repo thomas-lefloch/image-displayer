d27b60108a12475fc72fe55a8ebdc375094a3eb0
Le Floch Thomas <t.lefloch53@gmail.com>
2020-09-08 22:38:19 +0200

    cleaning

--- src/gui.hpp
+++ src/gui.hpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include "texture.hpp"
 
-// control panel action
-
 namespace Gui {
 
-enum CP_ACTION { NOOP, PREVIOUS, NEXT, ABORT, PLAY_PAUSE };
 void init_texture();
 bool input_dialog(std::string* filepath, int* timer);
+enum CP_ACTION { NOOP, PREVIOUS, NEXT, ABORT, PLAY_PAUSE };
 CP_ACTION control_panel(int time_left, const bool playing);
-
 static Texture play_texture, pause_texture, prev_texture, next_texture, close_texture;
+
 }
